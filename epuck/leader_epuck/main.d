4baa96ea2e58cb80c160aa909da07d4dbb60a116
Etienne Frederic Gabriel Thalmann <efgthalm@siepc-36.(none)>
2013-12-14 16:07:13 +0100

    Added leader epuck controller

---
+++ epuck/leader_epuck/main.d
@@ -0,0 +1,14 @@
+main.o: main.c \
+ ../../epuck/EpuckDevelopmentTree/library/std_microchip/support/h/p30f6014a.h \
+ ../../epuck/EpuckDevelopmentTree/library/std_microchip/include/stdio.h \
+ ../../epuck/EpuckDevelopmentTree/library/std_microchip/include/yvals.h \
+ ../../epuck/EpuckDevelopmentTree/library/std_microchip/include/string.h \
+ ../../epuck/EpuckDevelopmentTree/library/std_microchip/include/stdlib.h \
+ ../../epuck/EpuckDevelopmentTree/library/std_microchip/include/math.h \
+ ../../epuck/EpuckDevelopmentTree/library/./codec/e_sound.h \
+ ../../epuck/EpuckDevelopmentTree/library/./motor_led/e_init_port.h \
+ ../../epuck/EpuckDevelopmentTree/library/./motor_led/e_led.h \
+ ../../epuck/EpuckDevelopmentTree/library/./motor_led/e_motors.h \
+ ../../epuck/EpuckDevelopmentTree/library/./uart/e_uart_char.h \
+ ../../epuck/EpuckDevelopmentTree/library/./a_d/advance_ad_scan/e_prox.h \
+ ../../epuck/EpuckDevelopmentTree/library/./a_d/advance_ad_scan/e_ad_conv.h
